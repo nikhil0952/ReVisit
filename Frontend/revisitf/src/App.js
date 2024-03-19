b460bde3bf044cd8698125486586ff72def1af94
Nikhil Walia <nikhilwalia@Nikhils-MacBook-Air.local>
2024-03-19 11:05:07 +0530

    first commit

---
+++ Frontend/revisitf/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-blue-600">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
